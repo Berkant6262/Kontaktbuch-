b83c1f12e3a229cf22e514754aac7d4a77a011d0
Berkant6262 <Berkant6247_@outlook.com>
2025-05-18 17:34:36 +0200

    Tabelle Erstellung

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-def main():
-    print("Hallo Welt! Das ist dein Starter-Template.")
+import sqlite3
+
+
+def connect_db():
+    conn = sqlite3.connect("kontakte.db")  # Erstellt die Datei kontakte.db, wenn nicht da
+    cursor = conn.cursor()
+    # Tabelle Kontakte mit id, name, telefon, email
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS kontakte (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            telefon TEXT,
+            email TEXT
+        )
+    """)
+    conn.commit()
+    return conn
+
+
+
 
 if __name__ == "__main__":
-    main()
+    connect_db()
